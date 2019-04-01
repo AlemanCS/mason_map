51bc5795e4ef65d075d2e9a5d1d6d730b4d4c886
Elias Aleman <aleman.music@gmail.com>
2019-04-01 15:07:13 -0400

    Changed starting location of Marker to George Mason University

--- app/src/main/java/com/example/mason_map/MapsActivity.java
+++ app/src/main/java/com/example/mason_map/MapsActivity.java
@@ -37,7 +37,7 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near George Mason University.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -45,9 +45,9 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(38, -77);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker to George Mason and move the camera
+        LatLng georgeMason = new LatLng(38.8315, -77.3115);
+        googleMap.addMarker(new MarkerOptions().position(georgeMason).title("Best School ever"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(georgeMason));
     }
 }
