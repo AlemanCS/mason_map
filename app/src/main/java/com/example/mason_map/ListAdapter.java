022a1736a18b608a876abbeda3e2f2d686c5b112
Clayton Miller <cmille62@gmu.edu>
2019-04-02 18:26:04 -0400

    Added Link and changed text color, removed catagory / discription

--- app/src/main/java/com/example/mason_map/ListAdapter.java
+++ app/src/main/java/com/example/mason_map/ListAdapter.java
@@ -4,16 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.LayoutInflater;
+import android.net.Uri;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ListAdapter extends ArrayAdapter<Event>{
 
-    public Context context;
+    public final Context context;
     public int resource;
     private int lastPos;
 
@@ -24,6 +27,7 @@ public class ListAdapter extends ArrayAdapter<Event>{
         TextView start;
         TextView end;
         TextView location;
+        Button link;
     }
     public ListAdapter(Context context, int resource, ArrayList<Event> objects){
         super(context, resource, objects);
@@ -36,11 +40,10 @@ public class ListAdapter extends ArrayAdapter<Event>{
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         String title = this.getItem(position).getTitle();
-        //String description = this.getItem(position).getDescription();
-        //List category = this.getItem(position).getCategory();
         String start = this.getItem(position).getStart();
         String end = this.getItem(position).getEnd();
         String location = this.getItem(position).getLocation();
+        final String link = this.getItem(position).getLink();
 
         try {
             final EventHolder holder;
@@ -52,10 +55,10 @@ public class ListAdapter extends ArrayAdapter<Event>{
                 holder = new EventHolder();
 
                 holder.title = convertView.findViewById(R.id.eventTitle);
-                holder.category = convertView.findViewById(R.id.eventCatagory);
                 holder.start = convertView.findViewById(R.id.eventStart);
                 holder.end = convertView.findViewById(R.id.eventEnd);
                 holder.location = convertView.findViewById(R.id.eventLocation);
+                holder.link = convertView.findViewById(R.id.eventLink);
 
                 convertView.setTag(holder);
             }
@@ -65,9 +68,15 @@ public class ListAdapter extends ArrayAdapter<Event>{
             this.lastPos = position;
 
             holder.title.setText(title);
-            holder.start.setText(start);
-            holder.end.setText(end);
+            holder.start.setText("Starts: " + start);
+            holder.end.setText("Ends:" + end);
             holder.location.setText(location);
+            holder.link.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                    context.startActivity(browser);
+                }
+            });
         }
         catch(Exception exception){
             Log.e("List Adapter Error", "getView: ", exception);
